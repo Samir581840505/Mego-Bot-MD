ee61062ad45e6dfb96e798cba69ec3480669601d
Samir581840505 <jjyn87083@gmail.com>
2024-05-22 16:41:34 +0300

    Create عدم الاغلاق.js

---
+++ plugins/عدم الاغلاق.js
@@ -0,0 +1,24 @@
+import _0xc5dcb2 from "node-fetch";
+const handler = async (_0x5321d9, {
+  conn: _0x35ee5f,
+  args: _0x3a87c3,
+  usedPrefix: _0x1587d5,
+  command: _0x5403c9
+}) => {
+  let _0x206172 = _0x3a87c3.join(" ").split("|");
+  if * mego|bot";
+  }
+  _0x35ee5f.sendMessage(_0x5321d9.chat, {
+    react: {
+      text ,
+      key: _0x5321d9.key
+    }
+  });
+  let _0x361d0f = "" + _0x206172[0] + "&text2=" + _0x206172[1] + "&apikey=6wy2KEG8";
+  _0x35ee5f.sendFile(_0x5321d9", _0x5321d9, false);
+};
+handler.help = ["time 24housr./$"];
+handler.tags = ["up-time"];
+handler.command = /^(+14044000151)$/i;
+handler.limit = false;
+export default handler;
