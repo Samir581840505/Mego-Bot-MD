8fd249a2178d11bdefdbd01051f826aa6b872177
Samir581840505 <jjyn87083@gmail.com>
2024-05-24 11:28:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,18 @@ global.owner = [['201024480189', '🌩️ ẉa.me//𝐵𝛩𝑺𝑺‖.peị!‽
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '' //Ejemplo: +201024480189
 global.confirmCode = ''
 
 global.animxscans = ['201024480189']
 global.suittag = ['201024480189']
-global.mods = []
-global.prems = []
+global.mods = [201024480189]
+global.prems = [201024480189]
 
 global.packname = '『 +201024480189 』'
 global.author = '『 تــــربــــو بــ☠️ـوت 』'
 global.wm = '『تــــربــــؤؤ بــ🐍ـؤؤت 』'
+global.vs = 'V2 • 1.0.5':
 global.wm2 = '『 تــــربــــو بــ💎ـوت 』'
 global.azami = '『تــــربــــؤؤ بــ💀ـؤؤ 』'
 global.cb = '『 تــــربــــو بــ👾ـوت 』'
@@ -27,8 +28,8 @@ global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '© MEGO_BOT'
-global.devnum = '+201012531172'
+global.dev = '© TURBO_BOT'
+global.devnum = '+201024480189'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
